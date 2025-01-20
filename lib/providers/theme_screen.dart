304d64c9b2cd6c0139c2b0f099bd89f081dac8d6
Raül Lama <110611752+Raullam@users.noreply.github.com>
2025-01-20 20:29:17 +0100

    Update theme_screen.dart

--- lib/providers/theme_screen.dart
+++ lib/providers/theme_screen.dart
@@ -1,3 +1,9 @@
+// La classe ThemeProvider gestiona l'estat del tema de l'aplicació (llum o fosc).
+// Utilitza 'ChangeNotifier' per permetre que els widgets escoltin els canvis de tema.
+// El tema inicial es configura segons el valor de 'isDarkMode', i es pot canviar dinàmicament
+// entre mode clar (light) i mode fosc (dark) mitjançant els mètodes 'setLightMode' i 'setDarkMode'.
+// Quan el tema canvia, es crida 'notifyListeners' per actualitzar els widgets que depenen del tema.
+
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
