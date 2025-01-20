ee1ed03d3da6d04e27c22b5a3e0150e43709d80d
Raül Lama <110611752+Raullam@users.noreply.github.com>
2025-01-20 20:26:39 +0100

    Update direccions_screen.dart

--- lib/screens/direccions_screen.dart
+++ lib/screens/direccions_screen.dart
@@ -1,3 +1,7 @@
+// La classe DireccionsScreen és una pantalla que mostra una llista de codis QR escanejats 
+// de tipus 'http'. Utilitza el widget ScanTiles per mostrar els escaneigs filtrats per tipus 'http'.
+// Aquest tipus de codi QR normalment es fa servir per a enllaços web.
+
 import 'package:flutter/material.dart';
 import 'package:qr_scan/widgets/scan_tiles.dart';
 
