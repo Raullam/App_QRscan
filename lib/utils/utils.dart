af9f76c20a420d5ddf4ef0077cccc84429c39a38
Raül Lama <110611752+Raullam@users.noreply.github.com>
2025-01-20 20:31:38 +0100

    Update utils.dart

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,8 @@
+// La funció launchURL s'encarrega d'obrir un enllaç o una ubicació geogràfica segons el tipus del codi QR.
+// Si el tipus és 'http', intenta obrir l'URL utilitzant el mètode 'launch' de la llibreria 'url_launcher'.
+// Si el tipus és 'geo', navega a la pantalla de mapa ('mapa') i passa l'objecte ScanModel com a argument.
+// En cas que no es pugui obrir l'URL, es llança un error.
+
 import 'package:flutter/material.dart';
 import 'package:qr_scan/models/scan_model.dart';
 import 'package:url_launcher/url_launcher.dart';
