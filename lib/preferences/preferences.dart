dcf94a22e0462bdb239ccb54e47917e96f2f5f50
Raül Lama <110611752+Raullam@users.noreply.github.com>
2025-01-20 20:29:39 +0100

    Update preferences.dart

--- lib/preferences/preferences.dart
+++ lib/preferences/preferences.dart
@@ -1,3 +1,9 @@
+// La classe Preferences gestiona les preferències de l'usuari utilitzant SharedPreferences.
+// Permet emmagatzemar i recuperar el valor del mode fosc (dark mode) a través del mètode 'isDarkMode'.
+// El valor es guarda de manera persistent, de manera que es manté entre sessions de l'aplicació.
+// La funció 'init' inicialitza SharedPreferences abans d'accedir a les preferències.
+// El getter i setter 'isDarkMode' permeten llegir i escriure el valor del mode fosc a les preferències.
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Preferences {
