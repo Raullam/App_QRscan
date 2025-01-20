0d720d6ea4180af7a1a9593ac83c8f7af63ebca3
Raül Lama <110611752+Raullam@users.noreply.github.com>
2025-01-20 20:30:55 +0100

    Update ui_provider.dart

--- lib/providers/ui_provider.dart
+++ lib/providers/ui_provider.dart
@@ -1,3 +1,8 @@
+// La classe UiProvider gestiona l'estat de la selecció del menú a l'aplicació.
+// Manté una variable privada '_selectedMenuOpt' que emmagatzema l'índex de l'opció seleccionada del menú.
+// El getter 'selectedMenuOpt' retorna l'índex actual, mentre que el setter permet canviar-lo 
+// i notificar als widgets que depenen d'aquesta informació per actualitzar-se mitjançant 'notifyListeners'.
+
 import 'package:flutter/material.dart';
 
 class UiProvider extends ChangeNotifier {
