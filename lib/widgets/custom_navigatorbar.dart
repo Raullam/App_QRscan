b0dea64943ebe4ee512859c86b6af69415754ce1
Raül Lama <110611752+Raullam@users.noreply.github.com>
2025-01-20 20:32:16 +0100

    Update custom_navigatorbar.dart

--- lib/widgets/custom_navigatorbar.dart
+++ lib/widgets/custom_navigatorbar.dart
@@ -1,3 +1,8 @@
+// La classe CustomNavigationBar crea una barra de navegació inferior personalitzada.
+// Utilitza el proveïdor UiProvider per obtenir l'índex de l'opció seleccionada i actualitzar-lo quan l'usuari toca un element.
+// La barra de navegació té dues opcions: 'Mapas' i 'Direcciones', representades per icones corresponents.
+// Quan l'usuari selecciona una opció, es crida el setter 'selectedMenuOpt' del UiProvider per actualitzar l'índex de selecció.
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_scan/providers/ui_provider.dart';
